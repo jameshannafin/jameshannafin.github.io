55fb306436947fff73788a503cc44fe7b5f94de2
James Hannafin <93174542+jameshannafin@users.noreply.github.com>
2023-09-18 14:57:13 -0400

    Update main_sirensAbyss.js

--- JS/main_sirensAbyss.js
+++ JS/main_sirensAbyss.js
@@ -1,42 +1,34 @@
-
-
-// This function displays the first image in the slideshow when the page loads
 var slideIndex = 1;
+var slides = document.getElementsByClassName("slide"); // Declare slides outside the function
 show_slide(slideIndex);
 
-
-function slide_change(n) {// This function changes the slide when the left or right arrows are clicked
+function slide_change(n) {
     show_slide(slideIndex += n);
 }
 
-function slide_current(n) {// This function changes the slide when the dots are clicked
+function slide_current(n) {
     show_slide(slideIndex = n);
 }
 
-
-//This function is used to display the slide indicaded by 'n'
 function show_slide(n) {
-    var slides = document.getElementsByClassName("slide"); //Declare a var named slides and make its value an array of elements with the 'slide' sclass
-    var dots = document.getElementsByClassName("dot"); // This takes  elements with the  "dot" class and stores them in the variable array "dots"
-    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, the slideIndex is set to the length of the array "slides"
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 };
+    if (n < 1) { slideIndex = slides.length };
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
-    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 console.log("slideIndex:", slideIndex);
-console.log("slides:", slide);
 
 // Check if slides[slideIndex - 1] exists
 if (slides[slideIndex - 1]) {
-  slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 } else {
-  console.error("Slide element not found.");
+    console.error("Slide element not found.");
 }
-
